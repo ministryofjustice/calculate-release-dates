1ac0a0c268f018016a7b94acd35a0f489256f329
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2021-01-26 13:47:36 +0000

    Convert to arrow functions in utils (#14)
    * Convert to arrow functions in utils
    
    * Remove returns and fix prettier issues

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -1,10 +1,7 @@
-function properCase(word: string): string {
-  return word.length >= 1 ? word[0].toUpperCase() + word.toLowerCase().slice(1) : word
-}
+const properCase = (word: string): string =>
+  word.length >= 1 ? word[0].toUpperCase() + word.toLowerCase().slice(1) : word
 
-function isBlank(str: string): boolean {
-  return !str || /^\s*$/.test(str)
-}
+const isBlank = (str: string): boolean => !str || /^\s*$/.test(str)
 
 /**
  * Converts a name (first name, last name, middle name, etc.) to proper case equivalent, handling double-barreled names
@@ -12,10 +9,9 @@ function isBlank(str: string): boolean {
  * @param name name to be converted.
  * @returns name converted to proper case.
  */
-function properCaseName(name: string): string {
-  return isBlank(name) ? '' : name.split('-').map(properCase).join('-')
-}
+const properCaseName = (name: string): string => (isBlank(name) ? '' : name.split('-').map(properCase).join('-'))
 
-export default function convertToTitleCase(sentence: string): string {
-  return isBlank(sentence) ? '' : sentence.split(' ').map(properCaseName).join(' ')
-}
+const convertToTitleCase = (sentence: string): string =>
+  isBlank(sentence) ? '' : sentence.split(' ').map(properCaseName).join(' ')
+
+export default convertToTitleCase
