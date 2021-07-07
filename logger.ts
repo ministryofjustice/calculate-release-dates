53825ae5fbc8e43ed047c98db358bc96456ee0c4
arulelango-moj-digital <arulelango-moj-digital@users.noreply.github.com>
2021-07-07 13:46:40 +0000

    updating project name after deployment from template repository

--- logger.ts
+++ logger.ts
@@ -3,6 +3,6 @@ import bunyanFormat from 'bunyan-format'
 
 const formatOut = bunyanFormat({ outputMode: 'short', color: true })
 
-const logger = bunyan.createLogger({ name: 'HMPPS Typescript Template', stream: formatOut, level: 'debug' })
+const logger = bunyan.createLogger({ name: 'Calculate Release Dates', stream: formatOut, level: 'debug' })
 
 export default logger
