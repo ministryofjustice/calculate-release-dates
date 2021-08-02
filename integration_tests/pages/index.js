8f0ddae5c871aca0009528ebe994f11c51dcfddb
yasin <yasin.mustafa@gmail.com>
2021-08-02 16:32:33 +0100

    CRS-125 Fix integration tests

--- integration_tests/pages/index.js
+++ integration_tests/pages/index.js
@@ -1,7 +1,7 @@
 const page = require('./page')
 
 const indexPage = () =>
-  page('This site is under construction...', {
+  page('Calculate release dates', {
     headerUserName: () => cy.get('[data-qa=header-user-name]'),
   })
 
