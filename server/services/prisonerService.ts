49b52dc42826803e9c67b89a076bfabb0267b076
yasin <yasin.mustafa@gmail.com>
2021-08-02 15:50:50 +0100

    CRS-125 Integrate the prisoner search service and create some rudimentary screens for our service

--- server/services/prisonerService.ts
+++ server/services/prisonerService.ts
@@ -1,7 +1,9 @@
 import { Readable } from 'stream'
 import type HmppsAuthClient from '../api/hmppsAuthClient'
 import PrisonApiClient from '../api/prisonApiClient'
-import { PrisonApiPrisoner } from '../api/prisonClientTypes'
+import { PrisonApiPrisoner } from '../@types/prisonApi/prisonClientTypes'
+import PrisonerSearchApiClient from '../api/prisonerSearchApiClient'
+import { Prisoner, PrisonerSearchCriteria } from '../@types/prisonerOffenderSearch/prisonerSearchClientTypes'
 
 export default class PrisonerService {
   constructor(private readonly hmppsAuthClient: HmppsAuthClient) {}
@@ -14,5 +16,10 @@ export default class PrisonerService {
   async getPrisonerDetail(username: string, nomsId: string): Promise<PrisonApiPrisoner> {
     const token = await this.hmppsAuthClient.getSystemClientToken(username)
     return new PrisonApiClient(token).getPrisonerDetail(nomsId)
+  }
+
+  async searchPrisoners(username: string, prisonerSearchCriteria: PrisonerSearchCriteria): Promise<Prisoner[]> {
+    const token = await this.hmppsAuthClient.getSystemClientToken(username)
+    return new PrisonerSearchApiClient(token).searchPrisoners(prisonerSearchCriteria)
   }
 }
