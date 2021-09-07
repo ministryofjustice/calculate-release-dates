149d5246fb96eccb0a71d1e22a1beb162b180124
Yasin Mustafa <yasin.mustafa@gmail.com>
2021-09-07 08:52:28 +0100

    CRS-266 Change landing page to match the latest design (#25)

--- integration_tests/pages/index.js
+++ integration_tests/pages/index.js
@@ -3,6 +3,8 @@ const page = require('./page')
 const indexPage = () =>
   page('Calculate release dates', {
     headerUserName: () => cy.get('[data-qa=header-user-name]'),
+    mainHeading: () => cy.get('[data-qa=main-heading]'),
+    startNowButton: () => cy.get('[data-qa=start-now-button]'),
   })
 
 module.exports = { verifyOnPage: indexPage }
