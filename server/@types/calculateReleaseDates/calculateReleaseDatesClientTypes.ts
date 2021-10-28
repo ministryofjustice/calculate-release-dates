3ee64416f1f72e622b9e8baed4cca6244ab15149
Luke Harper <luke.harper@digital.justice.gov.uk>
2021-10-28 15:35:52 +0100

    CRS-499 weekend adjustment display.

--- server/@types/calculateReleaseDates/calculateReleaseDatesClientTypes.ts
+++ server/@types/calculateReleaseDates/calculateReleaseDatesClientTypes.ts
@@ -2,3 +2,4 @@ import { components } from './index'
 
 export type BookingCalculation = components['schemas']['BookingCalculation']
 export type TestData = components['schemas']['TestData']
+export type WorkingDay = components['schemas']['WorkingDay']
