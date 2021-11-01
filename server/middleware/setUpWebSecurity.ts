d39119398c7eaf9dd7fb8914434b82024c93ec75
Luke Harper <luke.harper@digital.justice.gov.uk>
2021-11-01 13:39:58 +0000

    CRS-443 google analytics implementation.

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -1,8 +1,15 @@
-import express, { Router } from 'express'
+import express, { Response, Router } from 'express'
 import helmet from 'helmet'
+import crypto from 'crypto'
+import { IncomingMessage } from 'http'
 
 export default function setUpWebSecurity(): Router {
   const router = express.Router()
+
+  router.use((req, res, next) => {
+    res.locals.cspNonce = crypto.randomBytes(16).toString('base64')
+    next()
+  })
 
   // Secure code best practice - see:
   // 1. https://expressjs.com/en/advanced/best-practice-security.html,
@@ -13,7 +20,15 @@ export default function setUpWebSecurity(): Router {
         directives: {
           defaultSrc: ["'self'"],
           // Hash allows inline script pulled in from https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/template.njk
-          scriptSrc: ["'self'", 'code.jquery.com', "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='"],
+          scriptSrc: [
+            "'self'",
+            'www.googletagmanager.com',
+            'www.google-analytics.com',
+            (req: IncomingMessage, res: Response) => `'nonce-${res.locals.cspNonce}'`,
+            'code.jquery.com',
+            "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
+          ],
+          connectSrc: ["'self'", 'www.googletagmanager.com', 'www.google-analytics.com'],
           styleSrc: ["'self'", 'code.jquery.com'],
           fontSrc: ["'self'"],
         },
