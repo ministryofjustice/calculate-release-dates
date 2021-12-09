f2f42a10ec159d4c2ecc113460dba14696397682
Luke Harper <luke.harper@digital.justice.gov.uk>
2021-12-09 14:52:59 +0000

    CRS-679 removing unused code

--- server/utils/errorUtils.ts
+++ server/utils/errorUtils.ts
@@ -1,29 +1,7 @@
 import ErrorResponse from '../@types/calculateReleaseDates/ErrorResponse'
 import GovUkError from '../@types/calculateReleaseDates/GovUkError'
 
-const errorCodeDefintions: Map<number, (e: ErrorResponse) => string> = new Map<number, (e: ErrorResponse) => string>([
-  [
-    1,
-    (e: ErrorResponse) => {
-      return `
-                <br/><br/>
-                ${e.arguments.join('<br/>')}<br/><br/>
-                
-                `.trim()
-    },
-  ],
-])
-
 export function serverErrorToGovUkError(errorResponse: ErrorResponse, href: string): GovUkError[] {
-  if (errorResponse.errorCode && errorCodeDefintions.has(errorResponse.errorCode)) {
-    const html = errorCodeDefintions.get(errorResponse.errorCode).call(this, errorResponse)
-    return [
-      {
-        html,
-        text: html,
-      },
-    ]
-  }
   return [
     {
       text: errorResponse.userMessage,
