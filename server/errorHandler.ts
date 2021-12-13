a8ad256293895c7210ecc574133b0ee4f54d36f3
Luke Harper <luke.harper@digital.justice.gov.uk>
2021-12-13 13:55:45 +0000

    CRS-574 adding 404 error page.

--- server/errorHandler.ts
+++ server/errorHandler.ts
@@ -1,15 +1,15 @@
 import type { Request, Response, NextFunction } from 'express'
+import { HttpError } from 'http-errors'
 import type { HTTPError } from 'superagent'
 import logger from '../logger'
 import { FullPageError, FullPageErrorType } from './types/FullPageError'
 
 export default function createErrorHandler(production: boolean) {
-  return (error: HTTPError | FullPageError, req: Request, res: Response, next: NextFunction): void => {
+  return (error: HTTPError | FullPageError | HttpError, req: Request, res: Response, next: NextFunction): void => {
     logger.error(`Error handling request for '${req.originalUrl}', user '${res.locals.user?.username}'`, error)
 
     if ('errorKey' in error) {
       res.locals.errorKey = FullPageErrorType[error.errorKey]
-      res.locals.status = error.status
     } else {
       if (error.status === 401 || error.status === 403) {
         logger.info('Logging user out')
@@ -19,9 +19,10 @@ export default function createErrorHandler(production: boolean) {
       res.locals.message = production
         ? 'Something went wrong. The error has been logged. Please try again'
         : error.message
-      res.locals.status = error.status
-      res.locals.stack = production ? null : error.stack
     }
+
+    res.locals.status = error.status
+    res.locals.stack = production ? null : error.stack
     res.status(error.status || 500)
     return res.render('pages/error')
   }
