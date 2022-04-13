ef932a7523bd83bc4e1f686ba9efdd26c63db289
Luke Harper <luke.harper@digital.justice.gov.uk>
2022-04-13 14:42:31 +0100

    CRS-846 fixed

--- server/routes/calculationRoutes.ts
+++ server/routes/calculationRoutes.ts
@@ -152,9 +152,7 @@ export default class CalculationRoutes {
   private async getBreakdownFragment(calculationRequestId: number, token: string): Promise<string> {
     return nunjucksEnv().render('pages/fragments/breakdownFragment.njk', {
       ...(await this.calculateReleaseDatesService.getBreakdown(calculationRequestId, token)),
-      showBreakdown() {
-        return true
-      },
+      showBreakdown: () => true,
     })
   }
 }
