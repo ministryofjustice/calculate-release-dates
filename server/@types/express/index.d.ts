f4d98468f769b5f09f6266bec829b59e4e927768
petergphillips <peter@greenthistle.co.uk>
2022-05-24 12:27:07 +0100

    SDI-211: 🔒️ Bump versions to fix security issues and cope with passport major upgrade (#96)

--- server/@types/express/index.d.ts
+++ server/@types/express/index.d.ts
@@ -19,6 +19,7 @@ export declare global {
     interface Request {
       verified?: boolean
       id: string
+      logout(done: (err: unknown) => void): void
     }
   }
 }
--- server/middleware/setUpAuthentication.ts
+++ server/middleware/setUpAuthentication.ts
@@ -31,13 +31,13 @@ export default function setUpAuth(): Router {
   const authUrl = config.apis.hmppsAuth.externalUrl
   const authSignOutUrl = `${authUrl}/sign-out?client_id=${config.apis.hmppsAuth.apiClientId}&redirect_uri=${config.domain}`
 
-  router.use('/sign-out', (req, res) => {
+  router.use('/sign-out', (req, res, next) => {
     if (req.user) {
-      req.logout()
-      req.session.destroy(() => res.redirect(authSignOutUrl))
-      return
-    }
-    res.redirect(authSignOutUrl)
+      req.logout(err => {
+        if (err) return next(err)
+        return req.session.destroy(() => res.redirect(authSignOutUrl))
+      })
+    } else res.redirect(authSignOutUrl)
   })
 
   router.use('/account-details', (req, res) => {
