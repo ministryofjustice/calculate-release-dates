127b2c7a74b3dbdc218835c3016adcd673efd858
petergphillips <peter@greenthistle.co.uk>
2022-06-08 10:17:20 +0100

    SDI-218: ⬆  Upgrade cypress to v10 (#101)

---
+++ cypress.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'cypress'
+import { resetStubs } from './integration_tests/mockApis/wiremock'
+import auth from './integration_tests/mockApis/auth'
+import tokenVerification from './integration_tests/mockApis/tokenVerification'
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  fixturesFolder: 'integration_tests/fixtures',
+  screenshotsFolder: 'integration_tests/screenshots',
+  videosFolder: 'integration_tests/videos',
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  },
+  videoUploadOnPasses: false,
+  taskTimeout: 60000,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on) {
+      on('task', {
+        reset: resetStubs,
+        ...auth,
+        ...tokenVerification,
+      })
+    },
+    baseUrl: 'http://localhost:3007',
+    excludeSpecPattern: '**/!(*.cy).ts',
+    specPattern: 'integration_tests/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'integration_tests/support/index.ts',
+  },
+})
