5341ec1f98d66333d114eb570a058b340f3e8325
ldlharper <luke.harper@digital.justice.gov.uk>
2022-06-22 13:34:11 +0100

    CRS-918 adding unit and integration tests. (#203)
    * CRS-918 adding unit and integration tests.
    
    * CRS-918 Fixing linting.
    
    * CRS-918 fixed unit tests.

---
+++ integration_tests/pages/selectOffences.ts
@@ -0,0 +1,32 @@
+import Page, { PageElement } from './page'
+
+export default class SelectOffencesPage extends Page {
+  constructor(list: string) {
+    super(`select-offences-${list}`)
+  }
+
+  public static goTo(prisonerId: string, list: string): SelectOffencesPage {
+    cy.visit(`/calculation/${prisonerId}/select-offences-that-appear-in-${list}`)
+    return new SelectOffencesPage(list)
+  }
+
+  public offenceListLink(): PageElement {
+    return cy.get(`[data-qa=offence-list-link]`)
+  }
+
+  public sentenceTable(caseSequence: number): PageElement {
+    return cy.get(`[data-qa=${caseSequence}-sentence-table]`)
+  }
+
+  public checkboxByIndex(index: number): PageElement {
+    return cy.get(`.row-checkbox:eq(${index})`)
+  }
+
+  public continueButton(): PageElement {
+    return cy.get(`[data-qa=submit-user-input]`)
+  }
+
+  public backLink(): PageElement {
+    return cy.get(`[data-qa=backlink]`)
+  }
+}
