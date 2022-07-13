137437da2404e28310df9973c9c26834611bc2d8
Joel Stobart (MOJ) <joel.stobart@digital.justice.gov.uk>
2022-07-13 20:35:45 +0100

    Update to redis

--- server/api/tokenStore.test.ts
+++ server/api/tokenStore.test.ts
@@ -1,34 +1,36 @@
-import { RedisClient } from 'redis'
+import { createClient } from 'redis'
 import TokenStore from './tokenStore'
 
-const redisClient = {
-  on: jest.fn(),
-  get: jest.fn(),
-  set: jest.fn(),
-}
+jest.mock('redis', () => ({
+  createClient: jest.fn().mockReturnValue({
+    on: jest.fn((eventName, listener) => {
+      return listener
+    }),
+    get: jest.fn().mockResolvedValue('token-1'),
+    set: jest.fn().mockResolvedValue('hello'),
+    connect: jest.fn(),
+  }),
+}))
 
 describe('tokenStore', () => {
-  let tokenStore: TokenStore
-
-  beforeEach(() => {
-    tokenStore = new TokenStore(redisClient as unknown as RedisClient)
-  })
+  const tokenStore: TokenStore = new TokenStore(createClient())
 
   afterEach(() => {
     jest.resetAllMocks()
   })
 
   it('Can retrieve token', async () => {
-    redisClient.get.mockImplementation((key, callback) => callback(null, 'token-1'))
+    const { redisClient } = tokenStore
 
     await expect(tokenStore.getToken('user-1')).resolves.toBe('token-1')
 
-    expect(redisClient.get).toHaveBeenCalledWith('user-1', expect.any(Function))
+    expect(redisClient.get).toHaveBeenCalledWith('user-1')
   })
 
   it('Can set token', async () => {
+    const { redisClient } = tokenStore
     await tokenStore.setToken('user-1', 'token-1', 10)
 
-    expect(redisClient.set).toHaveBeenCalledWith('user-1', 'token-1', 'EX', 10, expect.any(Function))
+    expect(redisClient.set).toHaveBeenCalledWith('user-1', 'token-1', { EX: 10 })
   })
 })
--- server/api/tokenStore.ts
+++ server/api/tokenStore.ts
@@ -1,35 +1,45 @@
-import redis from 'redis'
-import { promisify } from 'util'
+import { createClient } from 'redis'
 
 import logger from '../../logger'
 import config from '../config'
 
 const createRedisClient = () => {
-  return redis.createClient({
-    port: config.redis.port,
+  return createClient({
+    socket: {
+      port: config.redis.port,
+      host: config.redis.host,
+      tls: config.redis.tls_enabled === 'true',
+    },
     password: config.redis.password,
-    host: config.redis.host,
-    tls: config.redis.tls_enabled === 'true' ? {} : false,
-    prefix: 'systemToken:',
+    // prefix: 'systemToken:',
   })
 }
 
 export default class TokenStore {
-  private getRedisAsync: (key: string) => Promise<string>
+  private readonly getRedisAsync: (key: string) => Promise<string>
 
-  private setRedisAsync: (key: string, value: string, mode: string, durationSeconds: number) => Promise<void>
+  private readonly setRedisAsync: (key: string, value: string, mode: { EX: number }) => Promise<string>
 
-  constructor(redisClient: redis.RedisClient = createRedisClient()) {
-    redisClient.on('error', error => {
+  private readonly redisClientInternal
+
+  get redisClient() {
+    return this.redisClientInternal
+  }
+
+  constructor(redisClientImplementation = createRedisClient()) {
+    redisClientImplementation.on('error', (error: never) => {
       logger.error(error, `Redis error`)
     })
 
-    this.getRedisAsync = promisify(redisClient.get).bind(redisClient)
-    this.setRedisAsync = promisify(redisClient.set).bind(redisClient)
+    this.redisClientInternal = redisClientImplementation
+    this.getRedisAsync = this.redisClientInternal.get
+    this.setRedisAsync = this.redisClientInternal.set
   }
 
   public async setToken(key: string, token: string, durationSeconds: number): Promise<void> {
-    this.setRedisAsync(key, token, 'EX', durationSeconds)
+    await this.setRedisAsync(key, token, {
+      EX: durationSeconds,
+    })
   }
 
   public async getToken(key: string): Promise<string> {
