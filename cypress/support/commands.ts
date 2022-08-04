7fb3e475b0bca01b90c68f3109abdfbfbf4c7d9b
Luke Harper <luke.harper@digital.justice.gov.uk>
2022-08-04 12:42:17 +0100

    CRS-968 updating node packages complete.

--- cypress/support/commands.ts
+++
@@ -1,4 +0,0 @@
-Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
-  cy.request(`/`)
-  cy.task('getSignInUrl').then((url: string) => cy.visit(url, options))
-})
---
+++ integration_tests/support/commands.ts
@@ -0,0 +1,4 @@
+Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
+  cy.request('/')
+  return cy.task('getSignInUrl').then((url: string) => cy.visit(url, options))
+})
