f1e2bda987fee73d948ccdc639c9bf71ce29f040
Luke Harper <luke.harper@digital.justice.gov.uk>
2022-09-13 09:50:32 +0100

    CRS-1028 removing tests for dynamic content no longer applicable.

--- integration_tests/pages/alternativeReleaseIntro.ts
+++ integration_tests/pages/alternativeReleaseIntro.ts
@@ -10,22 +10,6 @@ export default class AlternativeReleaseIntroPage extends Page {
     return new AlternativeReleaseIntroPage()
   }
 
-  public listA(): PageElement {
-    return cy.get(`[data-qa=list-a]`)
-  }
-
-  public listB(): PageElement {
-    return cy.get(`[data-qa=list-b]`)
-  }
-
-  public listC(): PageElement {
-    return cy.get(`[data-qa=list-c]`)
-  }
-
-  public listD(): PageElement {
-    return cy.get(`[data-qa=list-d]`)
-  }
-
   public continueButton(): PageElement {
     return cy.get(`[data-qa=continue]`)
   }
