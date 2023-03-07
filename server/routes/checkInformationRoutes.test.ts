0c7ac400c958a7904f94f94abec4c5b5a5760e0e
Joel Stobart (MOJ) <joel.stobart@digital.justice.gov.uk>
2023-03-07 11:56:05 +0000

    Tweak to the language release vs removal

--- server/routes/checkInformationRoutes.test.ts
+++ server/routes/checkInformationRoutes.test.ts
@@ -370,7 +370,7 @@ describe('Check information routes tests', () => {
         expect(res.text).toContain('£3,000.00')
         expect(res.text).toContain('LR - EDS LASPO Discretionary Release')
         expect(res.text).not.toContain('987654')
-        expect(res.text).toContain('Include an Early release scheme eligibility date (ERSED) in this calculation')
+        expect(res.text).toContain('Include an Early removal scheme eligibility date (ERSED) in this calculation')
       })
   })
 
@@ -388,7 +388,7 @@ describe('Check information routes tests', () => {
       .expect(200)
       .expect('Content-Type', /html/)
       .expect(res => {
-        expect(res.text).not.toContain('Include an Early release scheme eligibility date (ERSED) in this calculation')
+        expect(res.text).not.toContain('Include an Early removal scheme eligibility date (ERSED) in this calculation')
       })
   })
   it('GET /calculation/:nomsId/check-information back button should reutrn to dps start page if no calc questions', () => {
