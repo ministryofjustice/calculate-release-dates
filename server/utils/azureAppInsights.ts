9ea30f9e4f5ee1c3adee73421bc38b05ee5f5dbf
Joel Stobart (MOJ) <87521440+joelstobart-moj@users.noreply.github.com>
2023-05-09 10:00:02 +0100

    Adding in appinsights configuration for calculate release dates (#368)

--- server/utils/azureAppInsights.ts
+++ server/utils/azureAppInsights.ts
@@ -1,6 +1,12 @@
 import { config } from 'dotenv'
-import { setup, defaultClient, TelemetryClient, DistributedTracingModes } from 'applicationinsights'
+import { Contracts, setup, defaultClient, TelemetryClient, DistributedTracingModes } from 'applicationinsights'
+import { EnvelopeTelemetry } from 'applicationinsights/out/Declarations/Contracts'
 import applicationVersion from '../applicationVersion'
+
+export type ContextObject = {
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  [name: string]: any
+}
 
 function defaultName(): string {
   const {
@@ -14,7 +20,7 @@ function version(): string {
   return buildNumber
 }
 
-export function initialiseAppInsights(): void {
+export function initialiseAppInsights(name = defaultName()): void {
   // Loads .env file contents into | process.env
   config()
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
@@ -22,6 +28,9 @@ export function initialiseAppInsights(): void {
     console.log('Enabling azure application insights')
 
     setup().setDistributedTracingMode(DistributedTracingModes.AI_AND_W3C).start()
+    defaultClient.context.tags['ai.cloud.role'] = name
+    defaultClient.context.tags['ai.application.ver'] = version()
+    defaultClient.addTelemetryProcessor(addUserDataToRequests)
   }
 }
 
@@ -32,4 +41,20 @@ export function buildAppInsightsClient(name = defaultName()): TelemetryClient {
     return defaultClient
   }
   return null
+}
+
+export function addUserDataToRequests(envelope: EnvelopeTelemetry, contextObjects: ContextObject): boolean {
+  const isRequest = envelope.data.baseType === Contracts.TelemetryTypeString.Request
+  if (isRequest) {
+    const { username } = contextObjects?.['http.ServerRequest']?.res?.locals?.user || {}
+    if (username) {
+      const { properties } = envelope.data.baseData
+      // eslint-disable-next-line no-param-reassign
+      envelope.data.baseData.properties = {
+        username,
+        ...properties,
+      }
+    }
+  }
+  return true
 }
