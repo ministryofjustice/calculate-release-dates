8259dda79f4371b4b5285c6d9cd70de3e043b4e6
yasin <yasin.mustafa@gmail.com>
2023-05-15 13:17:18 +0100

    CRS-1399 Add first draft of manual entry start page - will be used for maually entering release dates for unsupported sentences

--- server/routes/manualEntryRoutes.ts
+++ server/routes/manualEntryRoutes.ts
@@ -1,17 +1,22 @@
 import { RequestHandler } from 'express'
 import CalculateReleaseDatesService from '../services/calculateReleaseDatesService'
+import PrisonerService from '../services/prisonerService'
 
 export default class ManualEntryRoutes {
-  constructor(private readonly calculateReleaseDatesService: CalculateReleaseDatesService) {}
+  constructor(
+    private readonly calculateReleaseDatesService: CalculateReleaseDatesService,
+    private readonly prisonerService: PrisonerService
+  ) {}
 
   public landingPage: RequestHandler = async (req, res): Promise<void> => {
-    const { token } = res.locals.user
+    const { username, caseloads, token } = res.locals.user
     const { nomsId } = req.params
     const unsupportedSentenceOrCalculationMessages =
       await this.calculateReleaseDatesService.getUnsupportedSentenceOrCalculationMessages(nomsId, token)
     if (unsupportedSentenceOrCalculationMessages.length === 0) {
       return res.redirect(`/calculation/${nomsId}/check-information`)
     }
-    return res.render('pages/manualEntry/manualEntry', {})
+    const prisonerDetail = await this.prisonerService.getPrisonerDetail(username, nomsId, caseloads, token)
+    return res.render('pages/manualEntry/manualEntry', { prisonerDetail })
   }
 }
