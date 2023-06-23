9efe49460b2c505648f54366a0c5da461351cee6
Jamie Bowdidge <jamie.bowdige@digital.justice.gov.uk>
2023-06-23 11:19:20 +0100

    Remove DTO feature toggle

--- server/config.ts
+++ server/config.ts
@@ -102,7 +102,6 @@ export default {
   featureToggles: {
     edsSopcRecalls: get('EDS_SOPC_RECALLS', false) === 'true',
     ersed: get('ERSED_FEATURE_TOGGLE', false) === 'true',
-    dto: get('DTO_FEATURE_TOGGLE', false) === 'true',
     manualEntry: get('MANUAL_ENTRY_TOGGLE', false) === 'true',
   },
 }
