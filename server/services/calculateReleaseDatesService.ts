43c3dcf0d558257551704433d343498d08d2bdc9
jbowdidge-moj <122450608+jbowdidge-moj@users.noreply.github.com>
2023-10-13 18:05:00 +0100

    CRS-1636 show error if the data has changed since requestion GO (#485)

--- server/services/calculateReleaseDatesService.ts
+++ server/services/calculateReleaseDatesService.ts
@@ -60,9 +60,13 @@ export default class CalculateReleaseDatesService {
   async getCalculationResultsByReference(
     username: string,
     calculationReference: string,
-    token: string
+    token: string,
+    checkForChanges = false
   ): Promise<BookingCalculation> {
-    return new CalculateReleaseDatesApiClient(token).getCalculationResultsByReference(calculationReference)
+    return new CalculateReleaseDatesApiClient(token).getCalculationResultsByReference(
+      calculationReference,
+      checkForChanges
+    )
   }
 
   async getCalculationUserQuestions(prisonId: string, token: string): Promise<CalculationUserQuestions> {
