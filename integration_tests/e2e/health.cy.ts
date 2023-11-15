0c9c194af6e19a1902fcfb312ac8814d5d34db2a
petergphillips <peter@greenthistle.co.uk>
2023-11-15 16:28:50 +0000

    SDIT-1223: 🎨 Improve cypress healthcheck (#267)

--- integration_tests/e2e/health.cy.ts
+++ integration_tests/e2e/health.cy.ts
@@ -7,7 +7,7 @@ context('Healthcheck', () => {
       cy.task('stubTokenVerificationPing')
     })
 
-    it('Health check page is visible', () => {
+    it('Health check page is visible and UP', () => {
       cy.request('/health').its('body.status').should('equal', 'UP')
     })
 
@@ -21,16 +21,24 @@ context('Healthcheck', () => {
   })
 
   context('Some unhealthy', () => {
-    it('Reports correctly when token verification down', () => {
+    beforeEach(() => {
       cy.task('reset')
       cy.task('stubAuthPing')
+      cy.task('stubManageUsersPing')
       cy.task('stubTokenVerificationPing', 500)
+    })
 
+    it('Reports correctly when token verification down', () => {
       cy.request({ url: '/health', method: 'GET', failOnStatusCode: false }).then(response => {
         expect(response.body.components.hmppsAuth.status).to.equal('UP')
+        expect(response.body.components.manageUsersApi.status).to.equal('UP')
         expect(response.body.components.tokenVerification.status).to.equal('DOWN')
         expect(response.body.components.tokenVerification.details).to.contain({ status: 500, retries: 2 })
       })
+    })
+
+    it('Health check page is visible and DOWN', () => {
+      cy.request({ url: '/health', method: 'GET', failOnStatusCode: false }).its('body.status').should('equal', 'DOWN')
     })
   })
 })
