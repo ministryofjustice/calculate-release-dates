eae408fbef09349dd462297755ab88f18646286b
jbowdidge-moj <122450608+jbowdidge-moj@users.noreply.github.com>
2023-12-06 16:39:09 +0000

    CRS-1757 update sign-in callback to follow new pattern (#546)

--- server/middleware/setUpAuthentication.ts
+++ server/middleware/setUpAuthentication.ts
@@ -4,6 +4,7 @@ import passport from 'passport'
 import flash from 'connect-flash'
 import config from '../config'
 import auth from '../authentication/auth'
+import logger from '../../logger'
 
 const router = express.Router()
 
@@ -19,14 +20,39 @@ export default function setUpAuth(): Router {
     return res.render('autherror')
   })
 
-  router.get('/login', passport.authenticate('oauth2'))
+  router.get('/sign-in', passport.authenticate('oauth2'))
 
-  router.get('/login/callback', (req, res, next) =>
-    passport.authenticate('oauth2', {
-      successReturnToOrRedirect: req.session.returnTo || '/',
-      failureRedirect: '/autherror',
-    })(req, res, next)
-  )
+  router.get('/sign-in/callback', (req, res, next) => {
+    const authCallback: passport.AuthenticateCallback = (err, user, info) => {
+      if (err) {
+        logger.error('There was an error')
+        logger.error(err)
+        return res.redirect('/autherror')
+      }
+      if (!user) {
+        const { message } = info as { message: string }
+        if (info && message === 'Unable to verify authorization request state.') {
+          // failure to due authorisation state not being there on return, so retry
+          logger.error('Retrying auth callback as no state found')
+          return res.redirect('/')
+        }
+        logger.error(`Auth failure due to ${JSON.stringify(info)}`)
+        return res.redirect('/autherror')
+      }
+      const { returnTo } = req.session
+      req.logIn(user, err2 => {
+        if (err2) {
+          return next(err2)
+        }
+        if (typeof returnTo === 'string' && returnTo.startsWith('/')) {
+          return res.redirect(returnTo)
+        }
+        return res.redirect('/')
+      })
+      return null
+    }
+    passport.authenticate('oauth2', authCallback)(req, res, next)
+  })
 
   const authUrl = config.apis.hmppsAuth.externalUrl
   const authSignOutUrl = `${authUrl}/sign-out?client_id=${config.apis.hmppsAuth.apiClientId}&redirect_uri=${config.domain}`
