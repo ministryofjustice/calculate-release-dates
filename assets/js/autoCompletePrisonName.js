352646f93f1e80dedf44d491d51d5f2721464d91
Joel Stobart (MOJ) <joel.stobart@digital.justice.gov.uk>
2023-12-15 15:46:13 +0000

    Reflect the design in the prison selection

--- assets/js/autoCompletePrisonName.js
+++ assets/js/autoCompletePrisonName.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function () {
   accessibleAutocomplete.enhanceSelectElement({
     selectElement: document.querySelector('#prison-name'),
+    placeholder: 'Start typing the name of the prison',
   })
 })
