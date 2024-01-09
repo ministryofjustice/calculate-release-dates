a3ae83471b66238197d172ba1968be4e56d22648
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2024-01-09 13:09:49 +0000

    Upgrade to GovUK 5 and MoJ 2.0 (#297)
    * Upgrade to GovUK 5 and MoJ 2.0
    
    * Update sass command

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -31,8 +31,8 @@ export default function nunjucksSetup(app: express.Express, applicationInfo: App
   const njkEnv = nunjucks.configure(
     [
       path.join(__dirname, '../../server/views'),
-      'node_modules/govuk-frontend/',
-      'node_modules/govuk-frontend/components/',
+      'node_modules/govuk-frontend/dist/',
+      'node_modules/govuk-frontend/dist/components/',
       'node_modules/@ministryofjustice/frontend/',
       'node_modules/@ministryofjustice/frontend/moj/components/',
     ],
