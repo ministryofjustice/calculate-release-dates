8d701df2083ab74e25a554f79b4e851cf99ef7ef
petergphillips <peter@greenthistle.co.uk>
2024-01-10 11:18:16 +0000

    SDIT-1223: 🎨 Document timeout configuration options (#269)

--- server/config.ts
+++ server/config.ts
@@ -13,6 +13,7 @@ function get<T>(name: string, fallback: T, options = { requireInProduction: fals
 const requiredInProduction = { requireInProduction: true }
 
 export class AgentConfig {
+  // Sets the working socket to timeout after timeout milliseconds of inactivity on the working socket.
   timeout: number
 
   constructor(timeout = 8000) {
@@ -23,7 +24,11 @@ export class AgentConfig {
 export interface ApiConfig {
   url: string
   timeout: {
+    // sets maximum time to wait for the first byte to arrive from the server, but it does not limit how long the
+    // entire download can take.
     response: number
+    // sets a deadline for the entire request (including all uploads, redirects, server processing time) to complete.
+    // If the response isn't fully downloaded within that time, the request will be aborted.
     deadline: number
   }
   agent: AgentConfig
