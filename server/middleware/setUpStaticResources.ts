2afb1319e8947b9a2dfce35aa71d65a85cd79ae9
ushkarev <ushkarev@users.noreply.github.com>
2024-01-26 10:48:59 +0000

    Update MoJ frontend, dropping jQuery which is no longer required (#303)
    * Update MoJ frontend, dropping jQuery which is no longer required
    * Remove unused static assets route: `govuk_frontend_toolkit` is not currently included
    * Prevent renovate from updating `engines.node` and `engines.npm` in package.json because we’d usually want to stick to an LTS version rather than move to the latest

--- server/middleware/setUpStaticResources.ts
+++ server/middleware/setUpStaticResources.ts
@@ -21,17 +21,8 @@ export default function setUpStaticResources(): Router {
     '/node_modules/govuk-frontend/dist',
     '/node_modules/@ministryofjustice/frontend/moj/assets',
     '/node_modules/@ministryofjustice/frontend',
-    '/node_modules/jquery/dist',
   ).forEach(dir => {
     router.use('/assets', express.static(path.join(process.cwd(), dir), cacheControl))
-  })
-
-  Array.of('/node_modules/govuk_frontend_toolkit/images').forEach(dir => {
-    router.use('/assets/images/icons', express.static(path.join(process.cwd(), dir), cacheControl))
-  })
-
-  Array.of('/node_modules/jquery/dist/jquery.min.js').forEach(dir => {
-    router.use('/assets/js/jquery.min.js', express.static(path.join(process.cwd(), dir), cacheControl))
   })
 
   // Don't cache dynamic resources
