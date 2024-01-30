df60c7bc79f7d2192bc8834733b0965fd35e39da
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2024-01-30 10:26:15 +0000

    CRS-1729: Correct "Ineligible for HDCED" to "Ineligible for HDC".

--- server/@types/calculateReleaseDates/index.d.ts
+++ server/@types/calculateReleaseDates/index.d.ts
@@ -448,7 +448,7 @@ export interface components {
         | 'SDS_PLUS'
         | 'NEW_RULES_NOT_APPLIED'
         | 'FOURTEEN_DAY_RULE_NOT_APPLIED'
-        | 'INELIGIBLE_FOR_HDCED'
+        | 'INELIGIBLE_FOR_HDC'
         | 'RECALL_RELATED'
         | 'DATE_NOT_CALCULATED'
         | 'REMAND_OR_UAL_RELATED'
