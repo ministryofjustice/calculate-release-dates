0e6b85fb17cfa14d4f994af34fb7a80e8fd6a262
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2024-04-21 18:23:44 +0100

    Fixing linting issues

--- server/services/calculateReleaseDatesService.ts
+++ server/services/calculateReleaseDatesService.ts
@@ -36,9 +36,6 @@ import {
 import ErrorMessage from '../types/ErrorMessage'
 import { FullPageError } from '../types/FullPageError'
 import { AnalyzedPrisonApiBookingAndSentenceAdjustments } from '../@types/prisonApi/prisonClientTypes'
-import CalculationSummaryDatesCardModel, {
-  CalculationSummaryDatesCardLine,
-} from '../views/pages/components/calculation-summary-dates-card/CalculationSummaryDatesCardModel'
 
 export default class CalculateReleaseDatesService {
   // TODO test method - will be removed
