8ff31ba0ab1a5bda88a9ec87bb0f17e220318408
Luke Harper <luke.harper@digital.justice.gov.uk>
2024-07-30 16:19:39 +0100

    Fixed.

--- server/routes/checkInformationRoutes.ts
+++ server/routes/checkInformationRoutes.ts
@@ -68,7 +68,7 @@ export default class CheckInformationRoutes {
 
     const errors = await this.calculateReleaseDatesService.validateBackend(nomsId, userInputs, token)
     if (errors.messages.length > 0) {
-      if (errors.messageType === ErrorMessageType.UNSUPPORTED_SDS40_RECALL_SENTENCE_TYPE) {
+      if (errors.messageType === ErrorMessageType.UNSUPPORTED_SDS40_SENTENCE) {
         return res.redirect(`/calculation/${nomsId}/manual-entry`)
       }
       return res.redirect(`/calculation/${nomsId}/check-information?hasErrors=true`)
