f7e7d4017e7044645d24d39a6f819ad45a38a406
Joel Stobart <>
2024-09-26 11:52:26 +0100

    Removed a couple of tests

--- server/config.ts
+++ server/config.ts
@@ -130,8 +130,6 @@ export default {
     hdc4ComparisonTabEnabled: get('HDC4_PLUS_COMPARISON_TAB_ENABLED', false) === 'true',
     sdsExclusionIndicatorsEnabled: get('SDS_EXCLUSION_INDICATORS_ENABLED', false) === 'true',
     showBreakdown: get('SHOW_BREAKDOWN', false) === 'true',
-    sds40PolicySupported: get('SDS40_POLICY_SUPPORTED', 'OFF'),
-    showSDS40TrancheLabel: get('SHOW_SDS40_TRANCHE_LABEL', false) === 'true',
   },
   environmentName: get('ENVIRONMENT_NAME', ''),
   appInsightsConnectionString: get('APPLICATIONINSIGHTS_CONNECTION_STRING', '', requiredInProduction),
