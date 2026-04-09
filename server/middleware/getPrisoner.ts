22e77d9e9b869743166c288edaae1b456086e5c0
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2026-04-09 20:01:21 +0100

    Fix integration tests

--- server/middleware/getPrisoner.ts
+++ server/middleware/getPrisoner.ts
@@ -3,11 +3,10 @@ import PrisonerService from '../services/prisonerService'
 
 export default function getPrisoner(prisonerService: PrisonerService): RequestHandler {
   return async (req, res, next) => {
-    const { user } = res.locals
+    const { caseloads, userRoles, username } = res.locals.user
     const { nomsId } = req.params
 
-    if (user && nomsId) {
-      const { caseloads, userRoles, username } = res.locals.user
+    if (username && nomsId) {
       req.prisoner = await prisonerService.getPrisonerDetail(nomsId, username, caseloads, userRoles)
     }
     return next()
