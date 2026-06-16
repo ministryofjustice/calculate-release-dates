ea0d1a985fd30d0b898d5cfb366af01f9ecc23eb
mg-moj <matthew.green3@justice.gov.uk>
2026-06-16 07:31:31 +0100

    crs-1462-manual-journey-hdcad-validate (#1237)
    * crs-1462-manual-journey-hdcad-validate
    Where a user selects HDCAD as a date that needs to be entered, the user must also select HDCED.
    
    * crs-1462-manual-journey-hdcad-validate
    Where a user selects HDCAD as a date that needs to be entered, the user must also select HDCED.

--- server/services/manualEntryService.test.ts
+++ server/services/manualEntryService.test.ts
@@ -3,6 +3,8 @@ import ManualEntryService from './manualEntryService'
 import DateTypeConfigurationService from './dateTypeConfigurationService'
 import DateValidationService from './dateValidationService'
 import CalculateReleaseDatesService from './calculateReleaseDatesService'
+import { ErrorMessages } from '../types/ErrorMessages'
+import { ManualJourneySelectedDate } from '../types/ManualJourney'
 
 jest.mock('../services/calculateReleaseDatesService')
 jest.mock('../services/dateTypeConfigurationService')
@@ -91,5 +93,113 @@ describe('manualEntryService', () => {
       'or',
       'None',
     ])
+  })
+
+  describe('validateHdcadWithHdced', () => {
+    it('should add a validation message if HDCAD is selected without HDCED', () => {
+      const existingDates: ManualJourneySelectedDate[] = []
+      const selectedDateTypes = ['HDCAD']
+      const firstLoad = false
+      const validationMessages: ErrorMessages = { messages: [], messageType: null }
+      manualEntryService.validateHdcadWithHdced(existingDates, selectedDateTypes, firstLoad, validationMessages)
+      expect(validationMessages.messages).toEqual([{ text: 'HDCED must be selected with HDCAD' }])
+    })
+
+    it('should add a validation message if HDCAD is in existing dates and HDCED is not selected', () => {
+      const existingDates = [{ dateType: 'HDCAD' }] as ManualJourneySelectedDate[]
+      const selectedDateTypes: string[] = []
+      const firstLoad = false
+      const validationMessages: ErrorMessages = { messages: [], messageType: null }
+      manualEntryService.validateHdcadWithHdced(existingDates, selectedDateTypes, firstLoad, validationMessages)
+      expect(validationMessages.messages).toEqual([{ text: 'HDCED must be selected with HDCAD' }])
+    })
+
+    it('should not add a validation message if HDCAD and HDCED are both selected', () => {
+      const existingDates: ManualJourneySelectedDate[] = []
+      const selectedDateTypes = ['HDCAD', 'HDCED']
+      const firstLoad = false
+      const validationMessages: ErrorMessages = { messages: [], messageType: null }
+      manualEntryService.validateHdcadWithHdced(existingDates, selectedDateTypes, firstLoad, validationMessages)
+      expect(validationMessages.messages).toEqual([])
+    })
+
+    it('should not add a validation message if only HDCED is selected', () => {
+      const existingDates: ManualJourneySelectedDate[] = []
+      const selectedDateTypes = ['HDCED']
+      const firstLoad = false
+      const validationMessages: ErrorMessages = { messages: [], messageType: null }
+      manualEntryService.validateHdcadWithHdced(existingDates, selectedDateTypes, firstLoad, validationMessages)
+      expect(validationMessages.messages).toEqual([])
+    })
+
+    it('should not add a validation message if neither HDCAD nor HDCED are selected', () => {
+      const existingDates: ManualJourneySelectedDate[] = []
+      const selectedDateTypes = ['CRD']
+      const firstLoad = false
+      const validationMessages: ErrorMessages = { messages: [], messageType: null }
+      manualEntryService.validateHdcadWithHdced(existingDates, selectedDateTypes, firstLoad, validationMessages)
+      expect(validationMessages.messages).toEqual([])
+    })
+
+    it('should not add a validation message if HDCAD is selected on first load', () => {
+      const existingDates: ManualJourneySelectedDate[] = []
+      const selectedDateTypes = ['HDCAD']
+      const firstLoad = true
+      const validationMessages: ErrorMessages = { messages: [], messageType: null }
+      manualEntryService.validateHdcadWithHdced(existingDates, selectedDateTypes, firstLoad, validationMessages)
+      expect(validationMessages.messages).toEqual([])
+    })
+  })
+
+  describe('validateRemovedDate', () => {
+    const nomsId = 'A1234BC'
+    let testReq: Request
+
+    beforeEach(() => {
+      testReq = {
+        user: {},
+        session: {
+          selectedManualEntryDates: {
+            [nomsId]: [],
+          },
+        },
+        query: {},
+        body: {},
+      } as unknown as Request
+    })
+
+    it('should return an error if no radio button is selected', () => {
+      const result = manualEntryService.validateRemovedDate(testReq, nomsId)
+      expect(result).toBe("You must select either 'Yes' or 'No'")
+    })
+
+    it("should return null if 'no' is selected", () => {
+      testReq.body = { 'remove-date': 'no' }
+      const result = manualEntryService.validateRemovedDate(testReq, nomsId)
+      expect(result).toBeNull()
+    })
+
+    it("should return null if 'yes' is selected for a non-HDCED date", () => {
+      testReq.body = { 'remove-date': 'yes' }
+      testReq.query.dateType = 'CRD'
+      const result = manualEntryService.validateRemovedDate(testReq, nomsId)
+      expect(result).toBeNull()
+    })
+
+    it('should return an error if trying to remove HDCED when HDCAD exists', () => {
+      testReq.body = { 'remove-date': 'yes' }
+      testReq.query.dateType = 'HDCED'
+      testReq.session.selectedManualEntryDates[nomsId] = [{ dateType: 'HDCAD' }] as ManualJourneySelectedDate[]
+      const result = manualEntryService.validateRemovedDate(testReq, nomsId)
+      expect(result).toBe('HDCED cannot be deleted because a HDCAD still exists. You must delete the HDCAD first.')
+    })
+
+    it('should return null if trying to remove HDCED when HDCAD does not exist', () => {
+      testReq.body = { 'remove-date': 'yes' }
+      testReq.query.dateType = 'HDCED'
+      testReq.session.selectedManualEntryDates[nomsId] = [{ dateType: 'CRD' }] as ManualJourneySelectedDate[]
+      const result = manualEntryService.validateRemovedDate(testReq, nomsId)
+      expect(result).toBeNull()
+    })
   })
 })
